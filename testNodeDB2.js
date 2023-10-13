bef863a8607d5ee74498df84380eaf02be591b9f
Anmol Sharma <95350969+anmol09000@users.noreply.github.com>
2023-10-14 00:59:24 +0530

    Update testNodeDB2.js

--- testNodeDB2.js
+++ testNodeDB2.js
@@ -7,116 +7,165 @@ app.use(function (req, res, next) {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
-    res.header(
+  res.header(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE, HEAD"
   );
   next();
 });
-var port =process.env.PORT||2410;
+var port = process.env.PORT || 2410;
 app.listen(port, () => console.log(`Node app listening on port ${port}!`));
 
 const { Client } = require("pg");
 const client = new Client({
-  user:"postgres",
-  password:62653903220906,
-  database:"postgres",
-  port:5432,
-  host:"db.gjzcvxpbdbslbpezuedu.supabase.co",
+  user: "postgres",
+  password: "62653903220906",
+  database: "postgres",
+  port: 5432,
+  host: "db.gjzcvxpbdbslbpezuedu.supabase.co",
   ssl: { rejectUnauthorized: false },
 });
-client.connect(function(res,error){
-  console.log(`connected!!!`);
+client.connect(function (error) {
+  if (error) {
+    console.error("Error connecting to the database:", error);
+  } else {
+    console.log("Connected to the database!");
+  }
 });
 
 app.get("/employees", function (req, res) {
-    let department = req.query.department;
-    let designation = req.query.designation;
-    let gender = req.query.gender;
-  
-  
-    let conditions = [];
-    let values = [];
-  
-    if (department) {
-      conditions.push("department = ?");
-      values.push(department);
-    }
-    if (designation) {
-        conditions.push("designation = ?");
-        values.push(designation);
-      }
-      if (gender) {
-        conditions.push("gender = ?");
-        values.push(gender);
-      }
-    
-      if (conditions.length > 0) {
-        const sql = `SELECT * FROM employees WHERE ` + conditions.join(" AND ");
-        client.query(sql, values, function (err, result) {
-    
-          if (err) res.status(400).send(err)
-          else res.send(result.rows);
-        client.end();
-        });
+  let department = req.query.department;
+  let designation = req.query.designation;
+  let gender = req.query.gender;
+
+  let conditions = [];
+  let values = [];
+
+  if (department) {
+    conditions.push("department = $1");
+    values.push(department);
+  }
+  if (designation) {
+    conditions.push("designation = $2");
+    values.push(designation);
+  }
+  if (gender) {
+    conditions.push("gender = $3");
+    values.push(gender);
+  }
+
+  if (conditions.length > 0) {
+    const sql = `SELECT * FROM employees WHERE ${conditions.join(" AND ")}`;
+    client.query(sql, values, function (err, result) {
+      if (err) {
+        res.status(404).send("No Data Found");
       } else {
-        const sql = `SELECT * FROM employees`;
-        client.query(sql, function (err, result) {
-    
-          if (err) { res.status(400).send(err);}
-          else res.send(result.rows);
-          client.end();
+        res.send(result.rows);
+      }
+    });
+  } else {
+    const sql = `SELECT * FROM employees`;
+    client.query(sql, function (err, result) {
+      if (err) {
+        res.status(404).send("No Data Found");
+      } else {
+        res.send(result.rows);
+      }
     });
   }
 });
-app.get("/employees/dept/:dept",function(req,res){
-    let dept=req.params.dept
-    const sql=`SELECT * FROM employees WHERE department=?`;
-    client.query(sql,dept,function(err,result){
-        if(err) res.status(400).send(err);
-        else res.send(result);
-    })
-})
-app.get("/employees/designation/:desg",function(req,res){
-    let desg=req.params.desg;
-    const sql=`SELECT * FROM employees WHERE designation=?`;
-    client.query(sql,desg,function(err,result){
-        if(err) res.status(400).send(err);
-        else res.send(result.rows);
-    })
+
+app.get("/employees/dept/:dept", function (req, res) {
+  let dept = req.params.dept;
+  const sql = `SELECT * FROM employees WHERE department=$1`;
+  client.query(sql, [dept], function (err, result) {
+    if (err) {
+      res.status(404).send(err);
+    } else {
+      res.send(result.rows);
+    }
+  });
 });
 
-app.post("/newEmp",function(req,res){
-    let body=Object.values(req.body);
-    const sql=`INSERT INTO employees(empCode,name,department,designation,salary,gender) VALUES (?,?,?,?,?,?)`
-    client.query(sql,body,function(err,result){
-        if(err) res.status(400).send(err);
-        else res.send( `${result.rowCount} insertion successful`)
-    })
+app.get("/employees/designation/:desg", function (req, res) {
+  let desg = req.params.desg;
+  const sql = `SELECT * FROM employees WHERE designation=$1`;
+  client.query(sql, [desg], function (err, result) {
+    if (err) {
+      res.status(404).send(err);
+    } else {
+      res.send(result.rows);
+    }
+  });
 });
-app.get("/employee/:empCode",function(req,res){
-    let empCode=req.params.empCode;
-    const sql=`SELECT * FROM employees WHERE empCode=?`;
-    client.query(sql,empCode,function(err,result){
-        if(err) res.status.apply(400).send(err);
-        else res.send(result.rows);
-    })
-})
-app.put("/employee/:empCode",function(req,res){
-    let empCode=req.params.empCode;
-    let body=req.body;
-    const sql=`UPDATE employees SET ? WHERE empCode=?`;
-    client.query(sql,[body,empCode],function(err,result){
-        if(err) res.status(400).send(err);
-        else res.send(`${result.rowCount} updation successful`);
-    })
-})
-app.delete("/employees/:empCode",function(req,res){
-    let empCode=req.params.empCode;
-    const sql=`DELETE FROM employees WHERE empCode=?`;
-    client.query(sql,empCode,function(err,result){
-        if(err) res.status(400).send(err);
-        else res.send(result.rowCount)
-    })
 
-})
+app.post("/newEmp", function (req, res) {
+  let body = Object.values(req.body);
+  const sql = `INSERT INTO employees(empCode, name, department, designation, salary, gender) VALUES ($1, $2, $3, $4, $5, $6)`;
+  client.query(sql, body, function (err, result) {
+    if (err) {
+      res.status(404).send(err);
+    } else {
+      const sql = `SELECT * FROM employees`;
+      client.query(sql, function (err, result) {
+        if (err) {
+          res.status(404).send(err);
+        } else {
+          res.send(result.rowCount.toString());
+        }
+      });
+    }
+  });
+});
+
+app.get("/employee/:empCode", function (req, res) {
+  let empCode = req.params.empCode;
+  const sql = `SELECT * FROM employees WHERE empCode=$1`;
+  client.query(sql, [empCode], function (err, result) {
+    if (err) {
+      res.status(404).send(err);
+    } else {
+      res.send(result.rows);
+    }
+  });
+});
+
+app.put("/employee/:empCode", function (req, res) {
+  let empCode = req.params.empCode;
+  let body = req.body;
+  const sql = `UPDATE employees SET name=$1, department=$2, designation=$3, salary=$4, gender=$5 WHERE empCode=$6`;
+  const values = [body.name, body.department, body.designation, body.salary, body.gender, empCode];
+  client.query(sql, values, function (err, result) {
+    if (err) {
+      res.status(404).send(err);
+    } else {
+      const sql = `SELECT * FROM employees`;
+      client.query(sql, function (err, result) {
+        if (err) {
+          res.status(404).send(err);
+        } else {
+          res.send(result.rowCount.toString());
+        }
+      });
+    }
+  });
+});
+
+app.delete("/employees/:empCode", function (req, res) {
+  let empCode = req.params.empCode;
+  const sql = `DELETE FROM employees WHERE empCode=$1`;
+  client.query(sql, [empCode], function (err, result) {
+    if (err) {
+      res.status(404).send(err);
+    } else {
+      const sql = `SELECT * FROM employees`;
+      client.query(sql, function (err, result) {
+        if (err) {
+          res.status(404).send(err);
+        } else {
+          res.send(result.rowCount.toString());
+        }
+      });
+    }
+  });
+});
